fd0e9a0d86841dc65325fa03a6ce2173af69a437
jaime-h <jaime.e.hernandez@gmail.com>
2016-10-19 21:11:14 -0500

    First Commit

--- PodTest/ViewController.swift
+++ PodTest/ViewController.swift
@@ -7,19 +7,49 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
+import Foundation
+import SwiftCharts
+
+
 
 class ViewController: UIViewController {
 
+    var phData = [Double]()
+    var resData = [Double]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TODO: - Create a new class for the retrival of data?
+        let myUrl: String = "https://grogdata.soest.hawaii.edu/poh/data/node-021/PH_EXT.json?minutes=10080"
+        
+        Alamofire.request(myUrl).responseJSON { (responseData) -> Void in
+            if ((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                // After serialization grab the data arrays - there are two of them....
+                self.resData = swiftyJsonVar["samples"]["ReceptionTime"].arrayObject as! [Double]
+                self.phData = swiftyJsonVar["samples"]["PH_EXT"].arrayObject as! [Double]
+
+                // loop through the arrays and write out the data....
+                
+                let NewFormat = DateFormatter()
+                NewFormat.dateFormat = "MM-dd-yyyy HH:mm:ss"
+                
+                for i in 0 ..< self.resData.count {
+                    // let TimeStamp : Double = self.resData[i]
+                    let NewDateTime = NSDate(timeIntervalSince1970: self.resData[i] as Double)
+                    let NewDateToo = NewFormat.string(from: NewDateTime as Date)
+                    print("Count is \(i) the PH Level is \(self.phData[i]) and the date is \(NewDateToo)")
+                    
+                    // TODO: - create an array to pass to the chart library...
+                    // TODO: Wire up the storyboard to accept the new functionality
+                    
+                }
+             }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment this line to define a global platform for your project
+ source 'htpps://github.com/CocoaPods/Specs.git'
+ platform :ios, '10.0'
+
+target 'PodTest' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Alamofire', '~> 4.0'
+  pod 'SwiftyJSON'
+  pod 'SwiftCharts', :git => 'https://github.com/i-schuetz/SwiftCharts.git'
+
+  # Pods for PodTest
+
+end
